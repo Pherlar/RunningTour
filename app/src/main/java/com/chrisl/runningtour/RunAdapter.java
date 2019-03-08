24f6f8e7b3b41a0baa4a3f7c571aa174cf4bb8e6
Chris <christopherlarmour@gmail.com>
2019-03-09 10:03:44 +1100

    Some UI changes, Added a run, got the nav intent working

--- app/src/main/java/com/chrisl/runningtour/RunAdapter.java
+++ app/src/main/java/com/chrisl/runningtour/RunAdapter.java
@@ -1,6 +1,8 @@
 package com.chrisl.runningtour;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -36,7 +38,7 @@ public class RunAdapter extends RecyclerView.Adapter<RunAdapter.MyViewHolder>
     public void onBindViewHolder (MyViewHolder holder, int position)
     {
         Run run = runList.get(position);
-        holder.runLength.setText(String.valueOf(run.getRunDistance()));
+        holder.runLength.setText(String.valueOf(run.getRunDistance())+" km");
         holder.runName.setText(run.getRunName());
         holder.runImage.setImageResource(run.getImageResourceID());
     }
@@ -71,8 +73,11 @@ public class RunAdapter extends RecyclerView.Adapter<RunAdapter.MyViewHolder>
         {
             int mPosition = getLayoutPosition();
             Run currentRun = runList.get(mPosition);
+            Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(currentRun.getNavigationString()));
+            mapIntent.setPackage("com.google.android.apps.maps");
+            itemView.getContext().startActivity(mapIntent);
 
-            Toast.makeText(v.getContext(), currentRun.getRunName(),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(v.getContext(), currentRun.getRunName(),Toast.LENGTH_SHORT).show();
 
         }
     }
--- app/src/main/java/com/chrisl/runningtour/ShortRunsFragment.java
+++ app/src/main/java/com/chrisl/runningtour/ShortRunsFragment.java
@@ -37,8 +37,6 @@ public class ShortRunsFragment extends Fragment
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-
-
         return rootView;
     }
 
@@ -46,7 +44,8 @@ public class ShortRunsFragment extends Fragment
     private void buildRunList()
     {
         runList = new ArrayList<>();
-        runList.add(new Run("Run1", 1.5, R.drawable.background_image, "https://www.google.com.au/maps/dir/-33.8581101,151.21536/-33.8600804,151.2227072/-33.8631271,151.2217689/-33.8596638,151.2226761/-33.8581078,151.2152171/@-33.8598843,151.2189876,17.62z/data=!4m2!4m1!3e2"));
-
+        runList.add(new Run("Botanic Blast", 1.5, R.drawable.background_image, "https://www.google.com.au/maps/dir/-33.8581101,151.21536/-33.8600804,151.2227072/-33.8631271,151.2217689/-33.8596638,151.2226761/-33.8581078,151.2152171/@-33.8598843,151.2189876,17.62z/data=!4m2!4m1!3e2"));
+        runList.add(new Run("Bridge Dash", 2.7,R.drawable.sydney_harbour_bridge, "https://www.google.com/maps/dir/Milsons+Point+Station,+Milsons+Point+NSW+2061/-33.8546212,151.2094913/-33.860883,151.2068382/Cahill+Expressway+Lookout,+Sydney+NSW/@-33.853855,151.208523,16z/data=!4m16!4m15!1m5!1m1!1s0x6b12ae8b18dd9acb:0x4e8f9030e179e59b!2m2!1d151.2118241!2d-33.8458572!1m0!1m0!1m5!1m1!1s0x6b12ae684e18c45d:0x58d3153c800c0dc8!2m2!1d151.2124704!2d-33.8614186!3e2"));
     }
 }
+;
